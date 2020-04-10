1748ad2a4f1580b84086ef1ee5c79166cfa9f93d
nico1988 <410827024@qq.com>
2020-04-11 07:51:44 +0800

    promise play

--- 06-async/promise-play.js
+++ 06-async/promise-play.js
@@ -1,16 +1,29 @@
-const p = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        let flag = Math.random()*10;
-        if (flag < 5) {
-            resolve(1);
-        } else {
-            reject(new Error('message'));
-        }
-    },1000)
-});
-p.then(success).catch(error);
+const p = function () {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            let flag = Math.random()*10;
+            if (flag < 5) {
+                resolve(1);
+            } else {
+                reject(new Error('message'));
+            }
+        },1000)
+    });
+};
+p()
+    .then(success)
+    .catch(error)
+    .then(res => {
+        console.log('res:::', res);
+    });
 function success(res) {
     console.log('res:::', res);
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('reading from db：：：');
+            resolve({id: 1, gitHubName: 'nico1988'});
+        })
+    });
 }
 function error(err) {
     console.log('err:::', err);
