be08f10f6ba5addd75ee3fb37fe41f14b67b22f5
nico1988 <410827024@qq.com>
2020-04-11 08:53:02 +0800

    promise parallel

--- 06-async/promise-parallel.js
+++ 06-async/promise-parallel.js
@@ -10,8 +10,28 @@ const p2 = new Promise((resolve, reject) => {
         resolve({id: 1, name: 'nico'});
     }, 1000);
 });
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('async 2:::')
+        reject({id: 3, name: 'nico'});
+    }, 1000);
+});
 
 Promise.all([p1, p2]).then(res => {
     console.log('async operation comming:::');
     console.log('res:::', res); // 这里返回的resolve在一个数组里边
+});
+
+Promise.all([p1, p2, p3]).then(res => {
+    console.log('async operation comming:::');
+    console.log('res:::', res); // 这里返回的resolve在一个数组里边
+}).catch(err => {
+    console.log('err:::', err);
+});
+
+Promise.race([p1, p2]).then(res => {
+    console.log('async operation comming:::');
+    console.log('res:::', res); // 这里返回的resolve在一个数组里边
+}).catch(err => {
+    console.log('err:::', err);
 });
